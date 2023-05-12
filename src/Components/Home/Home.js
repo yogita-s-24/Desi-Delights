9a2b5d3f5752aed38469a082e4eb47c0bd571e23
yogitashete2412 <yogitashete24@gmail.com>
2023-05-12 22:14:13 +0530

    fetch changes to : Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,1 +1,18 @@
 
+import React from 'react'
+
+import Navbar from '../Navbar/Navbar'
+
+import "./Home.css"
+
+
+function Home() {
+  return (
+    <div className="container">
+        <Navbar/>
+       <h2 >my Home Page</h2> 
+    </div>
+  )
+}
+
+export default Home
