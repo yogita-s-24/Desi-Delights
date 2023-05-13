edbe93393c098cd912209da09bd07d280ab4b5ef
yogitashete2412 <yogitashete24@gmail.com>
2023-05-13 14:35:41 +0530

    fetch changes to : bestseller component and heading added

--- src/Components/Bestseller/bestseller.js
+++ src/Components/Bestseller/bestseller.js
@@ -4,8 +4,8 @@ import "./Bestseller.css"
 function Bestseller() {
     return (
         <div className='container'>
-           
-
+            <h2 class="mt-4 text-center heading-bestseller">Bestsellers</h2>
+            
             </div>
 
     )
