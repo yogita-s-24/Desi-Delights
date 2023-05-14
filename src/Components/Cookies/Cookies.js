c6c90be0ae5f4ed3b28d884755c56501b4cf2e97
VishalGaikwad11 <gaikwad.vishal1106@gmail.com>
2023-05-14 20:18:32 +0530

    navbar add in cookie page

--- src/Components/Cookies/Cookies.js
+++ src/Components/Cookies/Cookies.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
 
 function Cookies() {
   return (
-    <div>Cookies</div>
+    <div>
+      <Navbar/>
+      Cookies</div>
   )
 }
 
