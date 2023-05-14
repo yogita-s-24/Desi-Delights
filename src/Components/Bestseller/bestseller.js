e4347f9211aa741d7e49ebb38258af6cef50e079
yogitashete2412 <yogitashete24@gmail.com>
2023-05-14 23:53:23 +0530

    card added of bestseller

--- src/Components/Bestseller/bestseller.js
+++ src/Components/Bestseller/bestseller.js
@@ -6,8 +6,13 @@ function Bestseller() {
     return (
         <div className='heading-container'>
             <h2 class="mt-4 text-center heading-bestseller">Bestsellers</h2>
-             <BestsellerCards name="Kaju Katli"  imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/k/a/kaju_katli_2_2.jpg"/>
-             <BestsellerCards name="Orange Barfi" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/o/r/orange_burfee_2__1.jpg" />
+            <div className='prod-grp'>
+             <BestsellerCards className="prod-cards" name="Kaju Katli"  imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/k/a/kaju_katli_2_2.jpg"/>
+             <BestsellerCards className="prod-cards" name="Orange Barfi" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/o/r/orange_burfee_2__1.jpg" />
+             <BestsellerCards className="prod-cards" name="Moti Choor Laddu" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/m/o/moti-choor-ladoo-250-frontv1.jpg" />
+             <BestsellerCards className="prod-cards" name="Orange Barfi" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/o/r/orange_burfee_2__1.jpg" />
+             <BestsellerCards className="prod-cards" name="Orange Barfi" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/o/r/orange_burfee_2__1.jpg" />
+             </div>
              </div>
 
     )
--- src/Components/BestsellerCards/bestsellercard.js
+++ src/Components/BestsellerCards/bestsellercard.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import "./bestsellercard.css"
 
-function bestsellercard({name,imgUrl}) {
+function bestsellercard({name,imgUrl,btn,price}) {
   return (
     <div className='bestsellerCard'>
       <img className="img-heading" src={imgUrl} alt="image" />     
       <h2 className="name-header">{name}</h2>
+      <h3 className="amount-txt">{price}</h3>
+      <button className="card-btn">{btn}</button>
     </div>
   )
 }
