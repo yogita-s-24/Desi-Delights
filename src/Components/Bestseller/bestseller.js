c2f05384c06914190a281c0d33e18882c8189f0d
yogitashete2412 <yogitashete24@gmail.com>
2023-05-15 18:21:52 +0530

    fetch changes to bestseller Component: changes added in css

--- src/Components/Bestseller/bestseller.js
+++ src/Components/Bestseller/bestseller.js
@@ -7,6 +7,7 @@ function Bestseller() {
 <>
 <div className='container'>
 <h2 class="mt-5 text-center heading-bestseller">Bestsellers</h2>
+
 <div className='product-group p-5 m-5'>
 <BestsellerCards className="product-menu" name="Kaju Katli" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/k/a/kaju_katli_2_2.jpg" price="₹ 270" btn="Add To Cart"/>
 <BestsellerCards className="product-menu" name="Moti Chur Laddu" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/m/o/moti-choor-ladoo-250-frontv1.jpg" price="₹ 175" btn="Add To Cart"/>
@@ -16,7 +17,7 @@ function Bestseller() {
 <div className='product-group p-5 m-5'>
 <BestsellerCards className="product-menu" name="Son Papdi(300 gms)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/c/h/chocolate_soan_papdi-compressor_2.jpg" price="₹ 100" btn="Add To Cart"/>
 <BestsellerCards className="product-menu" name="Assorted Dry Fruit" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/a/s/assorted-dry-fruit-fv1.jpg" price="₹ 300" btn="Add To Cart"/>
-<BestsellerCards className="product-menu" name="Fancy Dry fruit Gift Box(WW)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/1/_/1_72.jpg" price="₹ 300" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Fancy Dry fruit Gift Box(WW)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/1/_/1_72.jpg" price="₹ 375" btn="Add To Cart"/>
 </div>
 
 
@@ -24,12 +25,12 @@ function Bestseller() {
 <div className='product-group  p-5 m-5'>
 <BestsellerCards className="product-menu" name="Fruit Delight(Berry Blast)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/f/r/fruit_delight_front.jpg" price="₹ 300" btn="Add To Cart"/>
 <BestsellerCards className="product-menu" name="Vanila Milkshake -(180ml - Pack of 10)  " imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/v/a/vanila_milk_shake.jpg" price="₹ 300" btn="Add To Cart"/>
-<BestsellerCards className="product-menu" name="Kaju Katli" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/2/_/2_4_6.jpg" price="₹ 300" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Mango Drink - (150ml - Pack of 20)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/2/_/2_4_6.jpg" price="₹ 200" btn="Add To Cart"/>
 </div>
 
 <div className='product-group  p-5 m-5'>
-<BestsellerCards className="product-menu" name="Kaju Katli" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/f/r/fruit_delight_front.jpg" price="₹ 300" btn="Add To Cart"/>
-<BestsellerCards className="product-menu" name="Kaju Katli" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/v/a/vanila_milk_shake.jpg" price="₹ 300" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Classic Lassi - (180ml - Pack 0f 15)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/l/a/lassi_6.jpg" price="₹ 300" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Red Chilli Papad(200gms)" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/i/m/img-20230313-wa0008.jpg" price="₹ 300" btn="Add To Cart"/>
 <BestsellerCards className="product-menu" name="Kaju Katli" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/2/_/2_4_6.jpg" price="₹ 300" btn="Add To Cart"/>
 </div>
 
