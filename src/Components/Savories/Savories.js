4304d54c757ccd19d4ef6b2ae0ba499b25b144cc
rohanrandhave0904 <rohanrandhave1212@gmail.com>
2023-05-15 21:40:46 +0530

    changes in savories

--- src/Components/Savories/Savories.js
+++ src/Components/Savories/Savories.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import './Savories.css'
 import Navbar from '../Navbar/Navbar'
+import addIcons from "../../savouries.webp"
+import BestsellerCards from '../BestsellerCards/bestsellercard'
 
 function Savories() {
   return (
-    <div>
-        <Navbar/>
-        Savories
+    <div class="full-container">
+      <Navbar/>
+      <img src={addIcons} className='savy-img' />
+      <div clas="cards-savy">
+        <BestsellerCards name="bhuji sev" btn="add to cart" price="100 rs/kg" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/b/h/bhujia_1.jpg"/>
+        <BestsellerCards name="moong dal" btn="add to cart" price="75rs/kg" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/p/u/punjabi_papad.jpg" />
+      
+      </div>
     </div>
   )
 }
