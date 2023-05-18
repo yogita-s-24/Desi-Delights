f8506275eddca18e1b9fafc85fafd0fcc5aafc91
VishalGaikwad11 <gaikwad.vishal1106@gmail.com>
2023-05-18 21:07:54 +0530

    added new changes

--- src/Components/Cookies/Cookies.js
+++ src/Components/Cookies/Cookies.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import "./Cookies.css";
+import Footer from '../Footer/Footer'
+
 
 function Cookies() {
   return (
     <div>
       <Navbar/>
-      <img src="https://www.haldirams.com/media/catalog/tmp/category/cookies_2.jpg"  />
-      Cookies</div>
-
+      <img src="https://www.haldirams.com/media/catalog/tmp/category/cookies_2.jpg" alt=""  className="d-block w-100"/>
+      
+      <Footer/>
+      </div>
+      
   )
 }
 
