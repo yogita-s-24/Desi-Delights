a9ccaefd0d806f3697682d2b3107013faaf4b137
rohanrandhave0904 <rohanrandhave1212@gmail.com>
2023-05-19 23:59:15 +0530

    changes in gifting

--- src/Components/Gifting/Gifting.js
+++ src/Components/Gifting/Gifting.js
@@ -18,6 +18,21 @@ function Gifting() {
 <BestsellerCards className="product-menu" name="Marvari bhujia" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/m/a/marwari.jpg" price="₹ 550" btn="Add To Cart"/>
 <BestsellerCards className="product-menu" name="bikneri Bhujia" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/b/i/bikanari.jpg" price="₹ 225" btn="Add To Cart"/>
 </div>
+<div className='product-group p-5 m-5'>
+<BestsellerCards className="product-menu" name="Bhoondi bhujia" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/b/o/boondi_1.jpg" price="₹ 600" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Masala peanuats" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/m/a/masala_peanuts.jpg" price="₹ 450" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Salted peanuats" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/p/a/pack_of_1.jpg" price="₹ 325" btn="Add To Cart"/>
+</div>
+<div className='product-group p-5 m-5'>
+<BestsellerCards className="product-menu" name="All in One" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/a/l/all_in_one_4.jpg" price="₹ 250" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Aloo Bhujia" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/a/l/aloo_bhujiya.jpg" price="₹ 425" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Chana choor" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/c/h/chana_choor_7.jpg" price="₹ 325" btn="Add To Cart"/>
+</div>
+<div className='product-group p-5 m-5'>
+<BestsellerCards className="product-menu" name="Chatpata Dal" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/c/h/chatpata_dal_2.jpg" price="₹ 240" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Light Chiwada" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/l/i/lite_chiwda_4.jpg" price="₹ 130" btn="Add To Cart"/>
+<BestsellerCards className="product-menu" name="Farali Chiwada" imgUrl="https://www.haldirams.com/media/catalog/product/cache/d792111afa4c25819db06da8dd2e361f/0/3/03_1_.jpg" price="₹ 325" btn="Add To Cart"/>
+</div>
 
 
 </div>
