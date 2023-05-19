d229b5553812ae8c1c7ec4d587e2591f7012e063
yogitashete2412 <yogitashete24@gmail.com>
2023-05-19 08:04:53 +0530

    changes added in gift cards and build logic to inc and dec button

--- src/Components/Gifting/Gifting.js
+++ src/Components/Gifting/Gifting.js
@@ -1,242 +1,230 @@
-import "./Gifting.css";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import GiftCards from "../GiftCards/GiftCards";
 import Footer from "../Footer/Footer";
+import "./Gifting.css";
 
 function Gifting() {
-    return ( <
-        >
-        <
-        Navbar / >
-        <
-        img src = "https://www.haldirams.com/media/catalog/tmp/category/gifting_1.jpg" / >
-        <
-        div className = "container" >
-        <
-        h2 className = "mt-5 text-center heading-bestseller" > Gifting < /h2> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div> <
-        div className = "Product-group p-5 m-5" >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/karodpati-bhujia-1378.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/royal-relish-375.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/classic-relish-145.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        GiftCards ClassName = "product-menu"
-        imgurl = "https://www.haldirams.com/imperial-relish-1368.html"
-        name = "coca"
-        price = "300rs"
-        btn = "Add to Cart" /
-        >
-        <
-        /div>{" "} <
-        /div>{" "} <
-        Footer / >
-        <
-        />
-    );
+
+  return (
+    <>
+      <Navbar />
+      <img src="https://www.haldirams.com/media/catalog/tmp/category/gifting_1.jpg" />
+      <div className="container">
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>{" "}
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>{" "}
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>{" "}
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>
+        <div className="Product-group p-5 m-5">
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/karodpati-bhujia-1378.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/royal-relish-375.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/classic-relish-145.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+          <GiftCards
+            ClassName="product-menu"
+            imgurl="https://www.haldirams.com/imperial-relish-1368.html"
+            name="coca"
+            price="300rs" btn1="+" text="0" btn2="-"
+            btn="Add to Cart"
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Gifting;
