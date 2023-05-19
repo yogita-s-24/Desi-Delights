f0772217727141e038a5a7cada448c10b9cbace5
Varshavjadhav <91647253+Varshavjadhav@users.noreply.github.com>
2023-05-19 23:30:42 +0530

    About us page added

--- src/Components/Aboutcard/Aboutcard.js
+++ src/Components/Aboutcard/Aboutcard.js
@@ -1,7 +1,9 @@
+import { hover } from '@testing-library/user-event/dist/hover'
 import React from 'react'
 import './Aboutcard.css'
 
-function Aboutcard({img,name,icon1,icon2,icon3}) {
+
+function Aboutcard({img,name,para,hover,icon1,icon2,icon3}) {
 
   return (
     <>
@@ -10,12 +12,15 @@ function Aboutcard({img,name,icon1,icon2,icon3}) {
     <div className="row row-cols-1 row-cols-md-3 g-4 ">
   <div className="col col-menu">
     <div className="card menu-card">
-      <img src={img} className="card-img-top" alt="img"/>
-      <div className="card-body">
-        <h5 className="card-title card-font">{name}</h5>
-        <img src={icon1} className="card-text"/>
-        <img src={icon2} className="card-text"/>
-        <img src={icon3} className="card-text"/>
+      <img src={img} className="card-image" alt="img"/>
+      <div className="card-bodynew">{hover}
+        <h5 className="card-titlenew">{name}</h5>
+        <p className="card-description">{para}</p>
+        
+        <img src={icon1} className="card-textnew"/>
+        <img src={icon2} className="card-textnew"/>
+        <img src={icon3} className="card-textnew"/>
+
         
       </div>
     </div>
