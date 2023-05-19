cfa6389ab5a2f0c9f5f51053d92d7b890b2ea0f2
samiksha@112 <samikshapinagle@gmail.com>
2023-05-19 14:24:20 +0530

    signin component is added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,32 +1,65 @@
-
-import React from 'react'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 import Navbar from '../Navbar/Navbar'
-
 import "./Login.css"
 
+
 function Login() {
+  <Navbar/>
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
     <>
     <Navbar/>
     <br/><br/><br/><br/>
-    <div class="login-box">
-    <h1>Login</h1>
-   
-    <form>
-    <label>Email</label>
-     <input type="email" placeholder=""/>
-     <label>Password</label>
-     <input type="password" placeholder=""/>
-     <input type="button" value="Submit"/>
-   </form> 
-    <div class="link2">Not have an account?<a class="signup-a" href="signIn">SignIn here</a>  
-    </div>
+    <div className='login-box'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} required />
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+
+
+
+      </form>
+      <p className='link2'>
+        Already have an account?{''}
+        <Link to="/signin">SignIn here</Link></p> 
     </div>
     </>
-
-   
-  
-  )
+  );
 }
 
-export default Login
+export default Login;
+
+
+
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,44 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 import Navbar from '../Navbar/Navbar'
-
 import "./SignIn.css"
 
+
+
 function SignIn() {
+ 
+
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmedpassword, setConfirmedPassword] = useState('');
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmedPasswordChange = (e) => {
+    setConfirmedPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+ 
   return (
     <>
     <Navbar/>
     <br/><br/><br/><br/>
-    <div class="signup-box">
-    <h1>SignIn</h1>
-    <h4>It's free and only takes a minute</h4>
-    <form>
-    <label>First Name</label>
-     <input type="text" placeholder=""/>
-     <label>Last Name</label>
-     <input type="text" placeholder=""/>
-     <label>Email</label>
-     <input type="email" placeholder=""/>
-     <label>Password</label>
-     <input type="password" placeholder=""/>
-     <label>Confirmed Password</label>
-     <input type="password" placeholder=""/>
-     <input type="button" value="Submit"/>
-   
-    </form> 
-    <p>By clicking the SignIn button,you agree to our<br/>
-    <a href="#">Terms and Condition</a> and <a href="#" id="">Policy Privacy</a>
-    </p>
-    
+    <div className='signup-box'>
+      <h1>Sign In</h1>
+      <h4>It's free and only takes a minute</h4>
+      <form onSubmit={handleSubmit}>
 
-    <div class="link1">Already have an account?<a class="login-a" href="login">Login here</a>
-    </div> 
-    </div>  
+        <div>
+          <label>First Name:</label>
+          <input type="firstname" value={firstname} onChange={handleFirstNameChange} required />
+        </div>
+
+        <div>
+          <label>Last Name:</label>
+          <input type="lastname" value={lastname} onChange={handleLastNameChange} required />
+        </div>
+
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={handlePasswordChange} required />
+        </div>
+        <div>
+          <label>Confirmed Password:</label>
+          <input type="confirmedpassword" value={confirmedpassword} onChange={handleConfirmedPasswordChange} required />
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+
+
+      </form>
+      <p>By clicking the SignIn button,you agree to our<br />
+        <a href="#">Terms and Condition</a> and <a href="#" id="">Policy Privacy</a>
+      </p>
+      <p className='link1'>
+        Already have an account?{''}
+        <Link to="/login">Login here</Link></p> 
+    </div>
     
     </>
-
-   
-  
-  )
+  );
 }
 
-export default SignIn
+
+export default SignIn;
